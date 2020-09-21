955404ebb194cebf73ef65296f8923574263c8d0
Kilroy <jordans1@vt.edu>
2020-09-20 21:38:04 -0400

    times can now be saved and retrieved from local storage

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 $(document).ready(function(){
     //collect current hour as variable
     var hour = moment().hour();
-    console.log(hour);
+    var hourNine = "";
+    var hourTen = "";
+    var hourEleven = "";
+    var hourTwelve = "";
+    var hourThirteen = "";
+    var hourFourteen = "";
+    var hourFifteen = "";
+    var hourSixteen = "";
+    var hourSeventeen = "";
+
     //Set current time and day at top of page
     $("#currentDay").text(moment().format('MMMM Do YYYY, h:mm a'));
 
@@ -64,4 +73,88 @@ $(document).ready(function(){
     }
 
     checkHour();
+
+    //collect user submitted info from the textareas and submit them to local storage
+    $("#9").on("click", function(){
+        hourNine = JSON.stringify($(".inputNine").val());
+        localStorage.setItem("hourNine", hourNine);
+    })
+    $("#10").on("click", function(){
+        hourTen = JSON.stringify($(".inputTen").val());
+        localStorage.setItem("hourTen", hourTen);
+    })
+    $("#11").on("click", function(){
+        hourEleven = JSON.stringify($(".inputEleven").val());
+        localStorage.setItem("hourEleven", hourEleven);
+    })
+    $("#12").on("click", function(){
+        hourTwelve = JSON.stringify($(".inputTwelve").val());
+        localStorage.setItem("hourTwelve", hourTwelve);
+    })
+    $("#13").on("click", function(){
+        hourThirteen = JSON.stringify($(".inputThirteen").val());
+        localStorage.setItem("hourThirteen", hourThirteen);
+    })
+    $("#14").on("click", function(){
+        hourFourteen = JSON.stringify($(".inputFourteen").val());
+        localStorage.setItem("hourFourteen", hourFourteen);
+    })
+    $("#15").on("click", function(){
+        hourFifteen = JSON.stringify($(".inputFifteen").val());
+        localStorage.setItem("hourFifteen", hourFifteen);
+    })
+    $("#16").on("click", function(){
+        hourSixteen = JSON.stringify($(".inputSixteen").val());
+        localStorage.setItem("hourSixteen", hourSixteen);
+    })
+    $("#17").on("click", function(){
+        hourSeventeen = JSON.stringify($(".inputSeventeen").val());
+        localStorage.setItem("hourSeventeen", hourSeventeen);
+    })
+
+
+    //check local storage for values and populate them to page if they exist
+    function init(){
+        var recordedNine = JSON.parse(localStorage.getItem("hourNine"));
+        var recordedTen = JSON.parse(localStorage.getItem("hourTen"));
+        var recordedEleven = JSON.parse(localStorage.getItem("hourEleven"));
+        var recordedTwelve = JSON.parse(localStorage.getItem("hourTwelve"));
+        var recordedThirteen = JSON.parse(localStorage.getItem("hourThirteen"));
+        var recordedFourteen = JSON.parse(localStorage.getItem("hourFourteen"));
+        var recordedFifteen = JSON.parse(localStorage.getItem("hourFifteen"));
+        var recordedSixteen = JSON.parse(localStorage.getItem("hourSixteen"));
+        var recordedSeventeen = JSON.parse(localStorage.getItem("hourSeventeen"));
+
+        //populate text areas if information was retrieved from local storage
+        if(recordedNine != ""){
+            $(".inputNine").val(recordedNine);
+        }
+        if(recordedTen != ""){
+            $(".inputTen").val(recordedTen);
+        }
+        if(recordedEleven != ""){
+            $(".inputEleven").val(recordedEleven);
+        }
+        if(recordedTwelve != ""){
+            $(".inputTwelve").val(recordedTwelve);
+        }
+        if(recordedThirteen != ""){
+            $(".inputThirteen").val(recordedThirteen);
+        }
+        if(recordedFourteen != ""){
+            $(".inputFourteen").val(recordedFourteen);
+        }
+        if(recordedFifteen != ""){
+            $(".inputFifteen").val(recordedFifteen);
+        }
+        if(recordedSixteen != ""){
+            $(".inputSixteen").val(recordedSixteen);
+        }
+        if(recordedSeventeen != ""){
+            $(".inputSeventeen").val(recordedSeventeen);
+        }
+    }
+
+    init();
+
 });
